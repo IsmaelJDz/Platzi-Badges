7af93dec578b3e9d6034beb7ab48db8a975fbdcf
Rene Ismael <Ismael_br7@hotmail.com>
2019-05-06 00:45:23 -0500

    Agregando el componente de detalles y separando UI Components y Container Components en BadgeDetailsContainer y BadgeDetails

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import BadgeNew from '../pages/BadgeNew';
 import Badges from  '../pages/Badges';
 import NotFound from '../pages/NotFound';
 import BadgeEdit from '../pages/BadgeEdit';
+import BadgeDetails from '../pages/BadgeDetailsContainer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
+        <Route exact path="/badges/:badgeId" component={BadgeDetails} />
         <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
         <Route component={NotFound} />
       </Switch>
